34d441b4e3ab546d6d155a77bef982f6ad846674
abritten <abritten@ucsd.edu>
2021-06-11 23:22:04 -0700

    initial commit to jetsim for google colab. Operates locally an on Ggoogle drive. latest feature is virtual joystick.

---
+++ jetsim_local/postprocess_images.py
@@ -0,0 +1,48 @@
+xMain = 0.0
+yMain = 0.0
+counter = 0
+
+def onClick(event):
+    global xMain, yMain, counter
+    x, y = event.xdata, event.ydata
+    xMain = int(x)
+    yMain = int(y)
+    
+    now = str(datetime.now())
+    now = now.replace(" ", "")
+    now = now.replace(".", "")
+    now = now.replace(":", "-")
+
+    newName = str(xMain) + "_" + str(yMain) + "_" + str(now) + ".jpg"
+    
+    print(newName)
+    
+    os.rename(imageName,('./m1_images/' + newName)) #images, lane_change, lane_cone_avoid. I made difernet folders for each training scenario. 
+    
+    counter = counter + 1
+    print(str(counter) + " image(s) renamed")
+    plt.close()
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import glob
+from datetime import datetime
+
+list = glob.glob("./m1_images/*.jpg") #images, lane_change, lane_cone_avoid
+list.sort()
+print(str(len(list)) + " images total")
+
+for imageName in list:
+    name = imageName
+    img = mpimg.imread(imageName)
+    plt.connect('button_press_event', onClick)
+    imgplot = plt.imshow(img)
+    plt.show()
+
+
+
+
+
+
+
